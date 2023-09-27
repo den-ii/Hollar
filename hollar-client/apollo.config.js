a0740e52517b29e23887c9f700196da010a3c28a
Deni Ochiche <wisdomochiche@gmail.com>
2023-09-27 16:48:21 +0100

    Update apollo.config.js

--- hollar-client/apollo.config.js
+++ hollar-client/apollo.config.js
@@ -3,7 +3,7 @@ module.exports = {
     service: {
       name: 'my-app',
       // URL to the GraphQL API
-      url: 'http://localhost:3000/graphql'
+      url: `http://${process.env.URL}/graphql`
     },
     // Files processed by the extension
     includes: ['src/**/*.vue', 'src/**/*.js']
